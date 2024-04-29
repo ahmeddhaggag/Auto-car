61865d28a921ef4f2541227a7cfae777c4360eb2
MAAB ELSAYED <140725524+Maab-sa-moh@users.noreply.github.com>
2024-04-29 12:08:26 +0300

    Add files via upload

---
+++ UltrasonicSensor.c
@@ -0,0 +1,75 @@
+/*
+ *_*
+ * ultrasonicSensor.c
+ *
+ *  Created on: 28 April 2024
+ *      Author: Maab-Sayed 
+ */
+ #include "ultrasonicSensor.h"
+/* Function prototypes */
+void Timer1_Init(Timer1_Mode_t mode, Timer1_OCRA1_Config_t OCRA1_config, Timer1_OCRB1_Config_t OCRB1_config);
+void Timer1_ICU_SetCallBack(void (*callback_func)(void));
+void Timer1_SetCallBack_COMPA(void (*callback_func)(void));
+void Timer1_Interrupt_COMPA_enable(void);
+void Timer1_InputCpetureEdge(Timer1_ICU_Edge_t edge);
+void Timer1_ICU_InterruptEnable(void);
+void Timer1_ICU_InterruptDisnable(void);
+void _delay_us(uint16_t us);
+void Dio_Write(uint8_t pin, uint8_t value);
+
+static volatile uint16_t ut1, ut2; // Capture values for rising and falling edges
+static volatile uint8_t uflag = 0; // Flag for capture events (0: idle, 1: rising, 2: falling)
+static volatile uint16_t overflow_count = 0; // Counter for timer overflows
+
+static void Func_ICU(void) {
+    if (uflag == 0) {
+        ut1 = ICR1;
+        uflag = 1;
+        Timer1_InputCpetureEdge(FALLING_EDGE);  // Switch to capture falling edge
+    } else if (uflag == 1) {
+        ut2 = ICR1;
+        uflag = 2;
+        Timer1_ICU_InterruptDisnable(); // Disable ICU interrupt
+        TMR1_Interrupt_COMPA_Disable(); // Disable COMPARE_A interrupt (optional)
+    }
+}
+
+static void TMR_OVR(void) {
+    overflow_count++; // Increment overflow counter
+}
+
+void Ultrasonic_init(void) {
+    Timer1_Init(TMR1_PWM_FAST_MODE_OCR1A, OCRA1_DISCONNECTED, OCRB1_NON_INVERTING);
+    OCR1A = 20000; /* Set OCR1A value for desired pulse width (adjust as needed) */
+    Timer1_ICU_SetCallBack(Func_ICU);
+    Timer1_SetCallBack_COMPA(TMR_OVR); /* Set callback for COMPARE_A interrupt  */
+    TMR1_Interrupt_COMPA_enable();     /* Enable COMPARE_A interrupt */
+}
+
+uint8_t UltrasonicReadDistance(Ultrasonic_t *ultra) {
+    uint8_t distance = 0;
+    uint32_t total_time = 0;
+
+    uflag = 0; /* Reset flag */
+    overflow_count = 0; /* Reset overflow counter */
+
+    TCNT1 = 0; /* Clear Timer1 counter */
+    Dio_Write(PORTB_0, HIGH);  /* Trigger high pulse */
+    _delay_us(10);             /* Trigger pulse duration */
+    Dio_Write(PORTB_0, LOW);   /* Trigger low pulse */
+
+    Timer1_InputCpetureEdge(RISING_EDGE); /* Capture rising edge for pulse start */
+    Timer1_ICU_InterruptEnable();      /* Enable ICU interrupt */
+
+    while (uflag < 2) {  /* Wait for both rising and falling edges */
+        
+    }
+
+    total_time = (ut2 - ut1) + ((uint32_t)overflow_count * 65535); /* Calculate total time */
+
+    distance = total_time / 58; /* Convert time to distance */
+
+    Timer1_ICU_InterruptDisnable(); /* Disable ICU interrupt */
+
+    return distance;
+}
---
+++ UltrasonicSensor.h
@@ -0,0 +1,34 @@
+
+/*
+ *_*
+ * ultrasonicSensor.h
+ *
+ *  Created on: 28 April 2024
+ *      Author: Maab-Sayed 
+ */
+
+
+
+#ifndef HAL_ULTRASONICSENSOR_ULTRASONICSENSOR_H_
+#define HAL_ULTRASONICSENSOR_ULTRASONICSENSOR_H_
+
+#include "../../MCAL/Dio/Dio.h"
+#include "../../MCAL/Timer1/Timer1.h"
+
+/*
+-------------- this This header file defines the basic
+ interface for an ultrasonic sensor driver.
+ It provides functions to initialize
+ the sensor and read the distance to an object --------*/
+
+typedef struct{
+	channel_t Echo;
+	channel_t Trigger;
+}Ultrasonic_t; /*it represents the digital pin connected to the echo signal of the ultrasonic sensor */
+
+void Ultrasonic_init();  /* This function initializes the ultrasonic sensor */
+
+u8 UltrasonicReadDistance(Ultrasonic_t *ultra); /* defined unsigned int as u8 it should read the distance of ultrasonic  */
+
+
+#endif 
