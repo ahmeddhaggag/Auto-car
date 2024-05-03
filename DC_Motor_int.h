388d190a1f860d39044264262b1f790c888bce5d
ahmeddhaggag <157414131+ahmeddhaggag@users.noreply.github.com>
2024-05-03 22:18:38 +0300

    Add files via upload

---
+++ DC_Motor_int.h
@@ -0,0 +1,31 @@
+/*
+ * DC_Motor_int.h
+ *
+ *  Created on: May 3, 2024
+ *      Author: Ahmed D. Haggag
+ */
+
+#ifndef DC_MOTOR_INT_H_
+#define DC_MOTOR_INT_H_
+
+#define in_PORT DIO_PORTB
+#define speedR_PORT DIO_PORTB  //OC0 pin
+#define speedL_PORT DIO_PORTD  // OC2 pin
+
+#define in1_PIN DIO_PIN1
+#define in2_PIN DIO_PIN2
+#define in3_PIN DIO_PIN4
+#define in4_PIN DIO_PIN5
+
+#define speedR_PIN DIO_PIN3
+#define speedL_PIN DIO_PIN7
+
+
+
+
+
+
+
+
+
+#endif /* DC_MOTOR_INT_H_ */
---
+++ DC_Motor_program.c
@@ -0,0 +1,68 @@
+/*
+
+ * DC_Motor_program.c
+ *
+ *  Created on: May 3, 2024
+ *      Author: Ahmed D. Haggag
+ */
+#include"dio_int.h"
+#include"TIMER_interface.h"
+#include"DC_Motor_int.h"
+
+
+
+void Forward(){
+	DIO_voidSetPinValue(in_PORT, in1_PIN, DIO_HIGH);
+	DIO_voidSetPinValue(in_PORT, in2_PIN, DIO_LOW);
+	DIO_voidSetPinValue(in_PORT, in3_PIN, DIO_HIGH);
+	DIO_voidSetPinValue(in_PORT, in4_PIN, DIO_LOW);
+
+	Timer0_voidGeneratePWM(70);      //speedR
+	Timer2_voidGeneratePWM(70);       //speedL
+}
+
+
+void Backward(){
+	DIO_voidSetPinValue(in_PORT, in1_PIN, DIO_LOW);
+	DIO_voidSetPinValue(in_PORT, in2_PIN, DIO_HIGH);
+	DIO_voidSetPinValue(in_PORT, in3_PIN, DIO_LOW);
+	DIO_voidSetPinValue(in_PORT, in4_PIN, DIO_HIGH);
+
+	Timer0_voidGeneratePWM(70);      //speedR
+	Timer2_voidGeneratePWM(70);       //speedL
+}
+
+void Right(){
+	DIO_voidSetPinValue(in_PORT, in1_PIN, DIO_HIGH);
+	DIO_voidSetPinValue(in_PORT, in2_PIN, DIO_LOW);
+	DIO_voidSetPinValue(in_PORT, in3_PIN, DIO_HIGH);
+	DIO_voidSetPinValue(in_PORT, in4_PIN, DIO_LOW);
+
+	Timer0_voidGeneratePWM(80);      //speedR
+	Timer2_voidGeneratePWM(50);       //speedL
+}
+
+void Left(){
+	DIO_voidSetPinValue(in_PORT, in1_PIN, DIO_HIGH);
+	DIO_voidSetPinValue(in_PORT, in2_PIN, DIO_LOW);
+	DIO_voidSetPinValue(in_PORT, in3_PIN, DIO_HIGH);
+	DIO_voidSetPinValue(in_PORT, in4_PIN, DIO_LOW);
+
+	Timer0_voidGeneratePWM(50);      //speedR
+	Timer2_voidGeneratePWM(80);       //speedL
+}
+
+void Stop(){
+	DIO_voidSetPinValue(in_PORT, in1_PIN, DIO_LOW);
+	DIO_voidSetPinValue(in_PORT, in2_PIN, DIO_LOW);
+	DIO_voidSetPinValue(in_PORT, in3_PIN, DIO_LOW);
+	DIO_voidSetPinValue(in_PORT, in4_PIN, DIO_LOW);
+
+	Timer0_voidGeneratePWM(0);      //speedR
+	Timer2_voidGeneratePWM(0);       //speedL
+}
+
+
+
+
+
