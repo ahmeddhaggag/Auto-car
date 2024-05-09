94bfcfe9ec59bf28578b3185b8dd006112cc9d9b
MAAB ELSAYED <140725524+Maab-sa-moh@users.noreply.github.com>
2024-05-09 20:14:56 +0300

    Update TIMER_interface.h

--- TIMER_interface.h
+++ TIMER_interface.h
@@ -13,6 +13,16 @@
 #define OC1A_PIN 0       //PORTD PIN5
 #define OC1B_PIN 1       //PORTD PIN4
 
+#define TIMER1_SET_OCR1A_CHAN_MODE(_MODE)        (TCCR1A = TCCR1A | (_MODE))
+#define TIMER1_SET_OCR1B_CHAN_MODE(_MODE)        (TCCR1B = TCCR1B | (_MODE))
+
+
+#define TMR1_SET_MODE(_MODE)             (TCCR1A = TCCR1A+( _MODE&0x03),\ TCCR1B = TCCR1B + ((_MODE &0x0c)<<1))
+
+
+#define TMR1_SET_u8_PRESCALLER(_PRE)        (TCCR1B = TCCR1B + (_PRE))
+
+
 typedef enum{
 	TIMER1_NORMAL_MODE=0,
 	TIMER1_PWM_PHASE_CORRECT_8BIT_MODE, /*  Pulse Width Modulation (PWM) modes with varying resolutions (8-bit, 9-bit, 10-bit) */
@@ -32,10 +42,10 @@ typedef enum{
 	TIMER1_PWM_FAST_MODE_OCR1A
 }TIMER1_mode;
 typedef enum{
-	OCRA1_DISCONNECTED=0,
-	OCRA1_TOGGLE=0X40,
-	OCRA1_NON_INVERTING=0X80,
-	OCRA1_INVERTING=0XC0
+	OCRA1_DISCONNECTED = 0,
+	OCRA1_TOGGLE= 0X40,
+	OCRA1_NON_INVERTING = 0X80,
+	OCRA1_INVERTING = 0XC0
 }OCRA_Phase_fast_pwm;
 typedef enum{
 	OCRB1_DISCONNECTED=0,
@@ -49,16 +59,6 @@ typedef enum{
 	RISING_EDGE
 }Edge_t;
 
-#define TIMER1_OCR1A_CHAN_MODE(_MODE)        (TCCR1A = TCCR1A | (_MODE))
-#define TIMER1_OCR1B_CHAN_MODE(_MODE)        (TCCR1B = TCCR1B | (_MODE))
-
-
-#define TIMER1_SET_MODE(_SETMODE)             (TCCR1A = TCCR1A+( _MODE&0x03),\
-										   TCCR1B = TCCR1B + ((_MODE &0x0c)<<1))
-
-
-#define TIMER1_SET_PRESCALLER(_PREsc)        (TCCR1B = TCCR1B + (_PRE))
-
 
 
 
@@ -67,6 +67,22 @@ void Timer0_voidInit(void);
 u8 Timer0_u8OvSetCallBack(void (*Copy_pvCallBackFunc)(void));
 
 void Timer0_voidGeneratePWM(u8 Copy_u8DutyCycle);
+
+void Timer1_Init(uint8 mode ,OCRA_Phase_fast_pwm outputa,OCRB_Phase_fast_pwm outputb);
+
+void Timer1_Interrupt_Enable();
+
+void Timer1_Interrupt_Disable();
+
+void Timer1_Set_u8_CallBack(void (*tmr1_ptf)(void));
+
+void TIMER1_Set_OCR1A(uint16 Copy_u16Value);
+
+void TIMER1_Set_OCR1B(uint16 Copy_u16Value);
+
+void TIMER1_Set_ICR1(uint16 Copy_u16Value);
+
+void TIMER1_Set_TCNT1(uint16 Copy_u16Value);
 
 void Timer1_voidGeneratePWM(u8 Copy_u8Pin, u8 Copy_u8DutyCycle);
 
