7eac90e5b2972fccf03f5a2ccb1f313eae41aa0c
MAAB ELSAYED <140725524+Maab-sa-moh@users.noreply.github.com>
2024-05-09 18:44:31 +0300

    Update TIMER_config.h

--- TIMER_config.h
+++ TIMER_config.h
@@ -27,7 +27,18 @@
 #define TIMER0_u8_PRELOAD			0
 #define FAST_PWM 0
 #define PHASE_PWM 1
+#define TIMER1_PRESCALLER     TIMER1_8_PRESCALER
 
+
+#define TIMER1_DISABLE               0X00
+#define TIMER1_1_PRESCALER           0X01
+#define TIMER1_8_PRESCALER           0X02
+#define TIMER1_64_PRESCALER          0X03
+#define TIMER1_256_PRESCALER         0X04
+#define TIMER1_1024_PRESCALER        0X05
+
+#define TIMER1_EXITRNAL_RISING       0X07
+#define TIMER1_EXITRNAL_FALLING      0X06
 #define WGM_MODE FAST_PWM
 
 #endif
