b2c63e1221e1d95144c2d22b1878bfa325f14d4a
MAAB ELSAYED <140725524+Maab-sa-moh@users.noreply.github.com>
2024-05-09 21:16:29 +0300

    Update UltrasonicSensor.h

--- UltrasonicSensor.h
+++ UltrasonicSensor.h
@@ -15,7 +15,7 @@
 #include "bitmath.h"
 #include "Dio_int.h"
 #include "TIMER_interface.h"
-#include "TIMER_program.c "
+#include "TIMER_private.h"
 /*
 -------------- this This header file defines the basic
  interface for an ultrasonic sensor driver.
