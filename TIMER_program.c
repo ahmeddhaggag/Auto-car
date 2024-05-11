a7e6d858045bae59d63555e3a661510a2617e33a
ahmeddhaggag <157414131+ahmeddhaggag@users.noreply.github.com>
2024-05-11 15:15:50 +0300

    Update TIMER_program.c

--- TIMER_program.c
+++ TIMER_program.c
@@ -8,6 +8,7 @@
 /*******************************************************************/
 #include<math.h>
 #include<avr/io.h>
+#include<avr/interrupt.h>
 #include "STD_TYPES.h"
 #include "bitmath.h"
 
@@ -17,6 +18,9 @@
 #include "TIMER_config.h"
 
 
+u8 ovf_count = 0;
+u16 highEdge =0, lowEdge = 0;
+u8 flag = 0;
 
 void Timer0_voidGeneratePWM(u8 Copy_u8DutyCycle){
 	TCCR0 = 0;
@@ -81,7 +85,7 @@ void Timer1_voidGeneratePWM(u8 Copy_u8Pin, u8 Copy_u8DutyCycle){
 void Timer1_voidInputCapture(u8 Copy_u8Edge){
 	TCCR1A =0;
 	TCCR1B =0;
- //prescaler 8, tick time : 5 us
+ //prescaler 8, tick time : 1 us
 	CLR_BIT(TCCR1B, TCCR1B_CS10);
 	SET_BIT(TCCR1B, TCCR1B_CS11);
 	CLR_BIT(TCCR1B, TCCR1B_CS12);
@@ -92,30 +96,65 @@ void Timer1_voidInputCapture(u8 Copy_u8Edge){
 	}else{
 		CLR_BIT(TCCR1B, TCCR1B_ICES1);
 	}
-//	SET_BIT(TIMSK, 5);
+
+
+}
+
+void __vector_9(void) __attribute__((signal));  //Timer1 overflow interrupt
+void __vector_9(void){
+
+	ovf_count++;
+
+}
+
+void __vector_6(void) __attribute__((signal));   //Timer1 Capture Interrupt
+void __vector_6(void){
+	if(flag == 0){
+		highEdge = ICR1L;
+		highEdge+= (ICR1H<<8);
+		flag++;
+		Timer1_voidInputCapture(1);
+	}else{
+		lowEdge = ICR1L;
+	    lowEdge+= (ICR1H<<8);
+		flag++;
+	}
+
 
 }
 
 
-u16 Timer1_u16MeasurePulseDuration(){
-	u8 edge =0;
-	u16 high =0, low =0;
-		Timer1_voidInputCapture(edge++);
-			while((TIFR & (1<<ICF1)) == 0);
-			high = ICR1L;
-			high += (ICR1H<<8);
-			TIFR =  (1<<ICF1);
-			Timer1_voidInputCapture(edge++);
-			while((TIFR & (1<<ICF1)) == 0);
-			low = ICR1L;
-			low += (ICR1H<<8);
-			TIFR =  (1<<ICF1);
-			if(high < low){
-				high+= 65536;
-			}
 
-			return high - low;
+u32 Timer1_u32MeasurePulseDuration(){
+	ovf_count = 0;
+	flag = 0;
+	Timer1_voidInputCapture(0);
+	while(flag < 2);
+	return ovf_count*65535 + lowEdge - highEdge;
+
+
 }
+
+//u16 Timer1_u16MeasurePulseDuration(){
+//	u8 edge =0;
+//	//u16 high =0, low =0;
+//		Timer1_voidInputCapture(edge++);
+//			while((TIFR & (1<<ICF1)) == 0);
+//			high = ICR1L;
+//			high += (ICR1H<<8);
+//			TIFR =  (1<<ICF1);
+//			Timer1_voidInputCapture(edge++);
+//			while((TIFR & (1<<ICF1)) == 0);
+//			low = ICR1L;
+//			low += (ICR1H<<8);
+//			TIFR =  (1<<ICF1);
+//			if(lowEdge < highEdge){
+////				high+= 65536;
+//				return 65536 - highEdge + lowEdge;
+//			}
+//
+//			return lowEdge - highEdge;
+//}
 
 void Timer2_voidGeneratePWM(u8 Copy_u8DutyCycle){
 	TCCR2 = 0;
@@ -135,8 +174,4 @@ void Timer2_voidGeneratePWM(u8 Copy_u8DutyCycle){
 }
 
 
-//void _vector_7(void)__attribute__((signal));
-//void _vector_7(void){
-//
-//
-//}
+
